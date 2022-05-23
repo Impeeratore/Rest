965f43d631f5457c14c4e5ef39bf187acaaaf058
Matteo De Lorenzo <delorenzomatteo9@gmail.com>
2022-05-23 13:04:57 +0100

    Initial commit Matteo

---
+++ src/test/java/co/develhope/Restteam/RestTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.Restteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
